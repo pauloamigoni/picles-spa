9eb1b5860f2b7803862c5889213a9dc31499e60a
Paulo Amigoni <paulo.amigoni@gmail.com>
2024-04-15 22:24:29 -0300

    criação do componente button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { Button } from './components/common/Button'
+import { ButtonVariant } from './components/common/Button/Button.constants'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+     
+      <div style={{display: 'flex', gap: '10px', flexDirection: 'row', alignItems: 'center'}}>
+        {count}
+        <Button variant={ButtonVariant.Default} onClick={() => setCount((count) => count +1)}> Default </Button>
+        <Button variant={ButtonVariant.Outlined} onClick={() => setCount((count) => count +1)}> Outlined </Button>
+        <Button variant={ButtonVariant.Disabled} onClick={() => setCount((count) => count +1)}> Disabled </Button>
+        <Button variant={ButtonVariant.Text} onClick={() => setCount((count) => count +1)}> Text</Button>
+      </div>
+    
+
+   
   )
 }
---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { ButtonHTMLAttributes } from "react"
+import { ButtonVariant } from "./Button.constants"  
+import styles from './Button.module.css'
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariant
+}
+
+export function Button({ variant = ButtonVariant.Default ,children, ...rest}: Readonly<IButton>) {
+    let buttonClass = styles.buttonBase
+    switch(variant) {
+        case ButtonVariant.Default:
+            buttonClass += ` ${styles.buttonDefault}`
+            break
+        case ButtonVariant.Disabled:
+            buttonClass += ` ${styles.buttonDisabled}`
+            break
+        case ButtonVariant.Outlined:
+            buttonClass += ` ${styles.buttonOutlined}`
+            break
+        case ButtonVariant.Text:
+            buttonClass += ` ${styles.buttonText}`
+            break
+    
+    }
+    return <button {...rest} className={buttonClass} >{children}</button>
+    
+}
