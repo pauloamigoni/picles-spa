7816bf9e56a20cfb2e3f228ce69575ccbb6108df
Paulo Amigoni <paulo.amigoni@gmail.com>
2024-05-05 22:47:06 -0300

    alterando link

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Layout } from './layout';
 
 const queryClient = new QueryClient();
 
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        <Layout children={undefined} />
-      </RouterProvider>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
